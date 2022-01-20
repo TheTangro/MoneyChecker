020d48a5acba43a217b7971801a2f5e2ceeaac5c
mchekavyj <TheTangro@overdose.digital>
2022-01-20 17:19:59 +0300

    initial commit

---
+++ resources/js/Pages/Main/History.vue
@@ -0,0 +1,28 @@
+<template>
+    <Head title="History" />
+
+    <BreezeAuthenticatedLayout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        History
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import {Head, Link} from '@inertiajs/inertia-vue3';
+
+export default {
+    components: {
+        BreezeAuthenticatedLayout,
+        Head,
+        Link
+    }
+}
+</script>
